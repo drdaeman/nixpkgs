6660c0b875bda7da1d3f3afa325ed4448eed949d
Aleksey Zhukov <drdaeman@drdaeman.pp.ru>
2017-11-07 21:57:47 +0300

    k380-conf: (re)init

--- pkgs/os-specific/linux/k380-conf/default.nix
+++ pkgs/os-specific/linux/k380-conf/default.nix
@@ -4,12 +4,16 @@ stdenv.mkDerivation {
   name = "k380-conf-0.0.1";
   src = fetchgit {
     url = https://github.com/jergusg/k380-function-keys-conf;
-    rev = "b1798b8350b90e93ec20d420632a2ac07ad36be2";
-    sha256 = "11sydfzlb0avvqxk43wasq5g8hjmc1n3vwag791lgv5h7aq34a75";
+    rev = "ed8eb60709a14ff26a36f993f40fe26f7aa6543a";
+    sha256 = "01zdbrqq0f4lg7ywm9knddb2dd4iv8k4ggrh94q106gic04aiijn";
   };
 
   buildPhase = "./build.sh";
-  installPhase = "mkdir -p $out/sbin; install -v -m755 k380_conf $out/sbin";
+  installPhase = ''
+    mkdir -p $out/sbin $out/etc/udev/rules.d
+    install -v -m755 k380_conf $out/sbin
+    echo "KERNEL==\"hidraw*\", KERNELS==\"*:046D:B342.*\", RUN+=\"$out/sbin/k380_conf -f on -d '%E{DEVNAME}'\"" > $out/etc/udev/rules.d/90-k380.rules
+  '';
 
   meta = with stdenv.lib; {
     homepage = https://github.com/jergusg/k380-function-keys-conf;
